42f4a2f6f3f84b12481a0e40bc1a8c3c33e3cc0e
Flypans <sbkwon011@outlook.com>
2024-08-30 18:25:49 +0100

    Add base code for webcam using OpenCVsharp4

---
+++ openCV/Form1.cs
@@ -0,0 +1,99 @@
+﻿using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace openCV
+{
+    public partial class Form1 : Form
+    {
+        VideoCapture capture;
+        Mat frame;
+        Bitmap image;
+        private Thread camera;
+        int isCameraRunning = 0;
+
+        private void CaptureCamera()
+        {
+            camera = new Thread(new ThreadStart(CaptureCameraCallback));
+            camera.IsBackground = true;
+            camera.Start();
+        }
+
+        private void CaptureCameraCallback()
+        {
+            frame = new Mat();
+            capture = new VideoCapture();
+            capture.Open(0);
+            //capture.Open(1);
+            //capture.Open(2);
+
+            if (!capture.IsOpened())
+            {
+                MessageBox.Show("Cannot open the camera.Please check the camera index.");
+                return;
+            }
+
+            while (isCameraRunning == 1)
+            {
+                capture.Read(frame);
+                if (!frame.Empty())
+                {
+                    image = BitmapConverter.ToBitmap(frame);
+                    UpdatePictureBox(image);
+                    //pictureBox1.Image = image;
+                }
+                else
+                {
+                    Console.WriteLine("Empty frame detected.");
+                }
+            }
+            capture.Release();
+        }
+
+        private void UpdatePictureBox(Bitmap img)
+        {
+            if (pictureBox1.InvokeRequired)
+            {
+                pictureBox1.Invoke(new Action(() => pictureBox1.Image = img));
+            }
+            else
+            {
+                pictureBox1.Image = img;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text.Equals("Start"))
+            {
+                CaptureCamera();
+                button1.Text = "Stop";
+                isCameraRunning = 1;
+            }
+            else
+            {
+                if (capture.IsOpened())
+                {
+                    capture.Release();
+                }
+
+                button1.Text = "Start";
+                //isCameraRunning = 0;
+            }
+        }
+    }
+}
